fdadfbb8ee9686ba1aac4564233fa8b20d2138e2
Yash <randomyashh@gmail.com>
2024-08-29 14:31:23 +0530

    modified:Ball Hard

--- src/components/CustomisedProduct.tsx
+++ src/components/CustomisedProduct.tsx
@@ -1,106 +1,157 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "./ui/button";
 import { Input } from "@/components/ui/input";
 import { Plus, Minus } from "lucide-react";
+import { products } from "@wix/stores";
+import Add from "./Add";
 
-const colorOptions = [
-  { id: "black", name: "Black", hex: "#000000" },
-  { id: "silver", name: "Silver", hex: "#C0C0C0" },
-  { id: "graphite", name: "Graphite", hex: "#474B4E" },
-];
-const sizeOptions = ["S", "M", "L", "XL", "XXL"];
+export default function CustomisedProduct({
+  productId,
+  variants,
+  productOptions,
+}: {
+  productId: string;
+  variants: products.Variant[];
+  productOptions: products.ProductOption[];
+}) {
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: string;
+  }>({});
+  const [selectedVariant, setSelectedVariant] = useState<products.Variant>();
 
-export default function CustomisedProduct() {
-  const [selectedColor, setSelectedColor] = useState(colorOptions[0].id);
-  const [selectedSize, setSelectedSize] = useState("");
-  const [quantity, setQuantity] = useState(1);
+  useEffect(() => {
+    const variant = variants.find((v) => {
+      const variantChoices = v.choices;
+      if (!variantChoices) return false;
+      return Object.entries(selectedOptions).every(
+        ([key, value]) => variantChoices[key] === value
+      );
+    });
+    setSelectedVariant(variant);
+  }, [selectedOptions, variants]);
+
+  useEffect(() => {
+    //Initializing selected options with the first choice of each option
+    const initialOptions: { [key: string]: string } = {};
+    productOptions.forEach((option) => {
+      if (option.choices && option.choices.length > 0) {
+        initialOptions[option.name!] = option.choices?.[0]?.description ?? "";
+      }
+    });
+    setSelectedOptions(initialOptions);
+  }, [productOptions]);
+
+  const handleOptionSelect = (optionType: string, choice: string) => {
+    setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+  };
+
+  const isVariantInStock = (choices: { [key: string]: string }) => {
+    return variants.some((variants) => {
+      const variantChoice = variants.choices;
+
+      if (!variantChoice) return false;
+
+      return (
+        Object.entries(choices).every(
+          ([key, value]) => variantChoice[key] === value
+        ) &&
+        variants.stock?.inStock &&
+        variants.stock?.quantity &&
+        variants.stock?.quantity > 0
+      );
+    });
+  };
 
   return (
     <>
       <div className="space-y-4">
-        <h3 className="text-lg font-semibold">Choose a color</h3>
-        <RadioGroup
-          value={selectedColor}
-          onValueChange={setSelectedColor}
-          className="flex flex-col space-y-2"
-        >
-          {colorOptions.map((color) => (
-            <div key={color.id} className="flex items-center space-x-3">
-              <RadioGroupItem
-                value={color.id}
-                id={color.id}
-                className="sr-only"
-              />
-              <Label
-                htmlFor={color.id}
-                className={`flex items-center space-x-3 cursor-pointer p-2 rounded-md transition-all ${
-                  selectedColor === color.id
-                    ? "bg-gray-100"
-                    : "hover:bg-gray-50"
-                }`}
-              >
-                <div
-                  className={`w-6 h-6 rounded-full border-2 ${
-                    selectedColor === color.id
-                  }`}
-                  style={{ backgroundColor: color.hex }}
-                />
-                <span className="text-sm font-medium">{color.name}</span>
-              </Label>
-            </div>
-          ))}
-        </RadioGroup>
-      </div>
-
-      <div className="space-y-4">
-        <h3 className="text-lg font-semibold">Choose a Size</h3>
-        <div className="flex flex-wrap gap-2">
-          {sizeOptions.map((size) => (
-            <Button
-              key={size}
-              variant={selectedSize === size ? "default" : "outline"}
-              onClick={() => setSelectedSize(size)}
-              className="w-12 h-12"
+        {productOptions.map((option) => (
+          <div key={option.name}>
+            <h3 className="text-lg font-semibold">Choose a {option.name}</h3>
+            <RadioGroup
+              value={selectedOptions[option.name!] || ""}
+              onValueChange={(value) => handleOptionSelect(option.name!, value)}
+              className="flex flex-col space-y-2"
             >
-              {size}
-            </Button>
-          ))}
-        </div>
+              {option.choices?.map((choice) => {
+                const disabled = !isVariantInStock({
+                  ...selectedOptions,
+                  [option.name!]: choice.description!,
+                });
+
+                const selected =
+                  selectedOptions[option.name!] === choice.description;
+
+                const clickHandler = disabled
+                  ? undefined
+                  : () => handleOptionSelect(option.name!, choice.description!);
+
+                return (
+                  <div
+                    key={choice.value}
+                    className={`flex items-center space-x-3 ${
+                      disabled ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                  >
+                    <RadioGroupItem
+                      value={choice.description!}
+                      id={choice.value}
+                      disabled={disabled}
+                      className="sr-only"
+                    />
+                    <Label
+                      htmlFor={choice.value}
+                      className={`flex items-center space-x-3 cursor-pointer p-2 rounded-md transition-all ${
+                        selected ? "bg-gray-100" : "hover:bg-gray-50"
+                      }`}
+                      onClick={() => {
+                        if (!disabled) {
+                          handleOptionSelect(option.name!, choice.description!);
+                        }
+                      }}
+                    >
+                      {option.name === "Color" ? (
+                        <>
+                          {selected && (
+                            <div
+                              className={`w-6 h-6 rounded-full border-2 ${
+                                selected ? "border-gray-400" : ""
+                              }`}
+                              style={{
+                                backgroundColor: choice.value,
+                                cursor: disabled ? "not-allowed" : "pointer",
+                              }}
+                              onClick={!disabled ? clickHandler : undefined}
+                            />
+                          )}
+                          <span className="text-sm font-medium">
+                            {choice.description}
+                          </span>
+                        </>
+                      ) : (
+                        <span className="text-sm font-medium">
+                          {choice.description}
+                        </span>
+                      )}
+                    </Label>
+                  </div>
+                );
+              })}
+            </RadioGroup>
+          </div>
+        ))}
       </div>
 
-      <div className="space-y-4">
-        <h3 className="text-lg font-semibold">Quantity</h3>
-        {/* <div className="flex items-center">
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={decrement}
-            disabled={quantity === 1}
-            className="rounded-r-none"
-          >
-            <Minus className="h-4 w-4" />
-          </Button>
-          <Input
-            type="number"
-            value={quantity}
-            onChange={handleInputChange}
-            className="w-16 text-center items-center justify-center rounded-none"
-            min="1"
-            max="99"
-          />
-          <Button
-            variant="outline"
-            onClick={increment}
-            disabled={quantity === 99}
-            className="rounded-l-none"
-          >
-            <Plus className="h-4 w-4" />
-          </Button>
-        </div> */}
-      </div>
+      <Add
+        productId={productId}
+        variantId={
+          selectedVariant?._id || "00000000-000000-000000-000000000001"
+        }
+        stockNumber={selectedVariant?.stock?.quantity || 0}
+      />
     </>
   );
 }
